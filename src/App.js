e055f4eb1bd804993e6fb3da6979f648da96c772
AbsolucionArtistica <122127365+AbsolucionArtistica@users.noreply.github.com>
2023-04-27 01:15:30 +0000

    vista de producto individual

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+const Mujer = () => {
+    return (
+        <>
+            <div className="row">
+                <div className="col-5 d-flex justify-content-center"> <img className="imagenProducto" src="https://thumbs.dreamstime.com/b/parte-delantera-de-la-camiseta-algod%C3%B3n-blanco-masculino-una-macho-sobre-un-colgador-aislado-fondo-sin-imprimir-188080429.jpg" /></div>
+                <div className="col-4"><h1 className="fs-3">Polera Mujer<br /></h1><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-regular fa-star fs-4"></i>(4)<br />
+                    <div className="row"><div className="col-2 mx-2"><label for="inputTalla" className="form-label">Talla</label></div><div className="col-2 mx-2"><label for="inputCantidad" className="form-label">Cantidad</label></div><br /></div>
+                    <select id="inputTalla" className="form-select-sm mx-1 w-25"><option>Seleccione...</option><option>...</option></select><select id="inputCantidad" className="form-select-sm mx-1 w-25"><option>Seleccione...</option><option>...</option></select><br />
+                    <div className="row justify-content-center"><div className="col-12"><button type="button" className="btn btn-primary btn-sm m-2 botonNaranjo w-50">Agregar al carrito</button></div></div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-3 m-5 d-flex"><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-solid fa-star fs-4"></i><i className="fa-regular fa-star fs-4"></i><p> comentarios(4)</p></div>
+            </div>
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center">
+                    <h1>Recomendaciones</h1>
+                </div>
+                <div className="col-12 d-flex">
+                    <img className="imagenSugerenciaProducto m-1" src="https://www.shutterstock.com/image-vector/blank-tshirt-on-hanger-260nw-123155623.jpg" />
+                    <img className="imagenSugerenciaProducto m-1" src="https://www.shutterstock.com/image-vector/blank-tshirt-on-hanger-260nw-123155623.jpg" />
+                    <img className="imagenSugerenciaProducto m-1" src="https://www.shutterstock.com/image-vector/blank-tshirt-on-hanger-260nw-123155623.jpg" />
+                    <img className="imagenSugerenciaProducto m-1" src="https://www.shutterstock.com/image-vector/blank-tshirt-on-hanger-260nw-123155623.jpg" />
+                    <img className="imagenSugerenciaProducto m-1" src="https://www.shutterstock.com/image-vector/blank-tshirt-on-hanger-260nw-123155623.jpg" />
+                </div>
+            </div>
+        </>
+    )
 }
 
-export default App;
+export default Mujer
+
